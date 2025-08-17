5d5ffa44326b78d358f7cc11d58eca0ea1d6393f
Blake <caroline.francielli@hotmail.com>
2025-08-17 15:05:17 -0300

    chore(config): application.properties configuration

--- eureka-server/src/main/java/com/nttdata/microservices/eureka_server/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/nttdata/microservices/eureka_server/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.nttdata.microservices.eureka_server;
+package com.nttdata.microservices.eureka_server; 
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
