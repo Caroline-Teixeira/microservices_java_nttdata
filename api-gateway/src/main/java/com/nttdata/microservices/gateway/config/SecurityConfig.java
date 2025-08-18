686282899182f3bee7bbd28c0ad7e647f083ba07
Blake <caroline.francielli@hotmail.com>
2025-08-18 19:13:39 -0300

    feat(api): add api products

---
+++ api-gateway/src/main/java/com/nttdata/microservices/gateway/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.nttdata.microservices.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable()) // Desativa CSRF para APIs REST
+            .authorizeHttpRequests(authorize -> authorize
+                .anyRequest().authenticated() // Todas as requisições requerem autenticação
+            )
+            .httpBasic(Customizer.withDefaults()); // Habilita autenticação básica
+        return http.build();
+    }
+
+
+}
